357093107a7822e1bafa479dd0510a0bb635c797
shakerygo <shakerygo@gmail.com>
2024-12-06 22:34:39 +0100

    Template

---
+++ template.py
@@ -0,0 +1,21 @@
+import math
+import numpy as np
+
+## Fetch data
+file = "DayX_test.txt"
+
+with open(file, 'r') as f:
+    raw = [line.strip() for line in f]
+
+## Solutions
+def Part_One():
+    pass
+
+def Part_Two():
+    pass
+
+
+
+print("Part One:", Part_One())
+
+print("Part Two:", Part_Two())
