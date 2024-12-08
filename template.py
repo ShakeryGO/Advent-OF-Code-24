885383b8822cec89fe89955a9f3d776979e9a664
shakerygo <shakerygo@gmail.com>
2024-12-08 17:58:03 +0100

    change template

--- template.py
+++ template.py
@@ -2,20 +2,30 @@ import math
 import numpy as np
 
 ## Fetch data
-file = "DayX_test.txt"
+def Create_Data(day, test=False):
+    if test:
+        file = f"Day{day}_test.txt"
+    else:
+        file = f"Day{day}.txt"
 
-with open(file, 'r') as f:
-    raw = [line.strip() for line in f]
+    with open(file, 'r') as f:
+        raw = [line.strip() for line in f]
+
+    return raw
 
 ## Solutions
+
+# == Part One
 def Part_One():
     pass
 
+# == Part Two
 def Part_Two():
     pass
 
 
+data = Create_Data("",test=False)
 
-print("Part One:", Part_One())
+print("Part One:", Part_One(data))
 
-print("Part Two:", Part_Two())
+print("Part Two:", Part_Two(data))
